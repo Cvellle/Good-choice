357334d838b2d5be81f16bfeabfb63f44fa6cbc4
Nikola Cvetic <cuekisa@yahoo.com>
2021-01-20 23:13:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'front_end', 'build')));
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'front_end', 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
   });
   
 }
