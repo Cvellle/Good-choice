af64a1872a066eaf1db510fc9cd394934e0a3258
Nikola Cvetic <cuekisa@yahoo.com>
2021-01-20 18:37:56 +0100

    client

---
+++ client/src/features/header/Header.tsx
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from 'react-redux';
+import useReactRouter from 'use-react-router';
+import Grid from '@material-ui/core/Grid';
+import HomeIcon from '@material-ui/icons/Home';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+
+import {
+  loggedUser,
+  logOutUser
+} from '../login/loginSlice';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 0,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+  }),
+);
+
+export default function Header() {
+  const dispatch = useDispatch();
+  const loggedUserSelector = useSelector(loggedUser);
+  const { history } = useReactRouter()
+  const classes = useStyles();
+
+  function onLogout() {
+    dispatch(logOutUser())
+  };
+
+    return (
+      <div className="App-header">
+          {(loggedUserSelector.email !== "") ? (
+            <div className={classes.root} style={{ width: '95%' }}>
+              <Grid container spacing={3}>          
+                <Grid item xs={6}>
+                <Box display="flex" justifyContent="flex-start" style={{alignContent: "center"}}>
+                  <HomeIcon style={{alignSelf: "center"}}/>
+                  {loggedUserSelector.role !== "USER_BEGINNER" && (
+                  <span>
+                    <Link to="/add-new" style={{textDecoration: 'none'}}>
+                      <span>Add new advice</span>
+                    </Link>
+                  </span>)}
+                  </Box>
+                </Grid>
+                <Grid item xs={6}>
+                <Box display="flex" justifyContent="flex-end">
+                  <span onClick={onLogout}>Logout</span>
+                  </Box>
+                </Grid>
+              </Grid>
+            </div>
+          ) : (
+            <div>
+              <span>
+                <Link to="/signup" style={{textDecoration: 'none', color: 'white'}}>SignUp</Link>
+              </span>
+              <span>
+                <Link to="/login" style={{textDecoration: 'none', color: 'white'}}>Login</Link>
+              </span>
+            </div>
+          )}
+      </div>
+    );
+  }
