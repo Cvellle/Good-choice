de1611579f655f361b749d2ba3cb035a32db9818
Nikola Cvetic <cuekisa@yahoo.com>
2021-01-20 23:32:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,24 +12,23 @@ const path = require('path');
 const dotenv = require("dotenv");
 dotenv.config();
 
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client', 'build')));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-  });
-  
-}
-
-app.use(cors())
-require("./routes/api-routes")(app);
-require("./routes/advices-routes")(app);
-
 mongoose.connect(
   process.env.MONGODB_URI ||
     "mongodb+srv://cvele:cvelePass@posts.jzao1.mongodb.net/posts",
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
+
+app.use(cors())
+require("./routes/api-routes")(app);
+require("./routes/advices-routes")(app);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static( 'client/build' ));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
+    });
+}
 
 app.listen(PORT, function () {
   console.log(`??  ==> API Server now listening on PORT ${PORT}!`);
