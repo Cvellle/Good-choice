4789b57af4c76c5aa6a1437af24107f1fc623f19
Nikola Cvetic <cuekisa@yahoo.com>
2021-01-20 22:48:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
-
-  app.get('*', (request, response) => {
-    response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
+    //set static folder
+    app.use(express.static('client/build'));
 }
+app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 require("./routes/api-routes")(app);
 require("./routes/advices-routes")(app);
